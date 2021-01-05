1da08d0d096d14a8a919a1e139cb259ae399f006
ian-travers <mr.itpe@gmail.com>
2021-01-05 09:50:32 +0300

    Test contact form

---
+++ tests/Feature/ContactFormTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\ContactForm;
+use Livewire;
+use Tests\TestCase;
+
+class ContactFormTest extends TestCase
+{
+    /** @test */
+    function main_page_contains_contact_form_livewire_component()
+    {
+        $this->get('/')
+            ->assertSeeLivewire('contact-form');
+    }
+
+    /** @test */
+    function valid_form_is_submitted()
+    {
+        Livewire::test(ContactForm::class)
+            ->set('name', 'Onio')
+            ->set('email', 'sup@mail.com')
+            ->set('phone', '55547852')
+            ->set('message', 'This is great!')
+            ->call('submitForm')
+            ->assertSee('We received your message successfully and will get back to you shortly!');
+    }
+
+    /** @test */
+    function it_requires_a_name_an_email_a_phone_a_message()
+    {
+        Livewire::test(ContactForm::class)
+            ->call('submitForm')
+            ->assertHasErrors([
+                'name' => 'required',
+                'email' => 'required',
+                'phone' => 'required',
+                'message' => 'required',
+            ]);
+    }
+
+    /** @test */
+    function it_requires_a_name_min_3_characters()
+    {
+        Livewire::test(ContactForm::class)
+            ->set('name', 'On')
+            ->call('submitForm')
+            ->assertHasErrors([
+                'name' => 'min',
+            ]);
+    }
+
+    /** @test */
+    function it_requires_a_message_min_5_characters()
+    {
+        Livewire::test(ContactForm::class)
+            ->set('message', 'wow')
+            ->call('submitForm')
+            ->assertHasErrors([
+                'message' => 'min',
+            ]);
+    }
+
+    /** @test */
+    function it_requires_a_valid_email()
+    {
+        Livewire::test(ContactForm::class)
+            ->set('email', 'wow@mail')
+            ->call('submitForm')
+            ->assertHasErrors([
+                'email' => 'email',
+            ]);
+    }
+}
