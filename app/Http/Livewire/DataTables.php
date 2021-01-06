ae9b0e34201ca0f4a2e9195ed29698444fe1b369
ian-travers <mr.itpe@gmail.com>
2021-01-06 14:53:56 +0300

    Resetting Pagination After Filtering Data

--- app/Http/Livewire/DataTables.php
+++ app/Http/Livewire/DataTables.php
@@ -13,6 +13,11 @@ class DataTables extends Component
     public $active = true;
     public $search;
 
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
         return view('livewire.data-tables', [
