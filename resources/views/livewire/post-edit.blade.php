04b4c15f8433d2a9c2b3b9088d69cb4ef30017c1
ian-travers <mr.itpe@gmail.com>
2021-01-12 10:30:46 +0300

    Image loading spinner

--- resources/views/livewire/post-edit.blade.php
+++ resources/views/livewire/post-edit.blade.php
@@ -85,7 +85,7 @@
                     Cover Photo
                 </label>
                 <div
-                    class="mt-2 sm:mt-0 sm:col-span-2"
+                    class="mt-2 sm:mt-0 sm:col-span-2 relative"
                 >
                     <input
                         wire:model="photo"
@@ -95,6 +95,26 @@
                     @error('photo')
                     <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
                     @enderror
+                    <div class="absolute top-0 -left-5">
+                        <svg
+                            wire:loading
+                            wire:target="photo"
+                            class="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-600"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                class="opacity-25" cx="12" cy="12" r="10"
+                                stroke="currentColor" stroke-width="4">
+
+                            </circle>
+                            <path
+                                class="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                            </path>
+                        </svg>
+                    </div>
                     <div class="mt-4">
                         @if($photo)
                             <img src="{{ $photo->temporaryUrl() }}" alt="temp">
